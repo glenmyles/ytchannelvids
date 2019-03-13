01b0aee56f192094b54d199ee7e5ec7737dd4e05
glenmyles <glenmyles@users.noreply.github.com>
2019-03-13 11:27:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 require 'Classes/Videos.class.php';
 
 $videos = new Videos;
-$videos->setAPIKey("AIzaSyA9qwDtn_QWUmr4HzQee_vL96T1a-ilcPA");
-$videos->setChannelName("ActualDannyGonzalez");
+$videos->setAPIKey("{__GOOGLE_API_KEY__}");
+$videos->setChannelName("{_YOUTUBE_CHANNEL_}");
 $videos->init();
 var_dump($videos->getVideos());
 
